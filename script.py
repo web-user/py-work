c884f181a3c3aaf308fa44ee94e8aa626549a457
web-user <pva2687@gmail.com>
2019-04-12 16:55:36 +0300

    add test

--- script.py
+++ script.py
@@ -2,12 +2,12 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class PythonLogin(unittest.TestCase):
+class PythonTest(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome('/usr/bin/chromedriver')
 
-    def test_in_python_org(self):
+    def test_in_web_page_com(self):
         driver = self.driver
         # for Authentication http://user:pass@test.web.page.com/
         driver.get("http://test.web.page.com/")
@@ -19,9 +19,35 @@ class PythonLogin(unittest.TestCase):
         loginbutton = driver.find_element_by_xpath("//input[@type='submit']")
         loginbutton.click()
 
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+    def test_search_by_text(self):
+        # navigate to the application home page
+        self.driver.get("http://www.google.com/")
+        # get the search textbox
+        self.search_field = self.driver.find_element_by_name("q")
+
+        # enter search keyword and submit
+        self.search_field.send_keys("Selenium WebDriver Interview questions")
+        self.search_field.submit()
+
+        #get the list of elements which are displayed after the search
+        #currently on result page usingfind_elements_by_class_namemethod
+
+        lists = self.driver.find_elements_by_class_name("r")
+        no=len(lists)
+        self.assertEqual(10, len(lists))
 
     def tearDown(self):
         self.driver.close()
+
 
 if __name__ == "__main__":
     unittest.main()
